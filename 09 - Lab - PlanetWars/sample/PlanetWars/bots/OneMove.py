521df74247392d3cd0eb9ccab2a0914c17e93424
James Bonner <jbonner@swin.edu.au>
2024-04-16 10:42:35 +1000

    Fixed gameinfo references in Planetwars OneMove bot

--- 09 - Lab - PlanetWars/sample/PlanetWars/bots/OneMove.py
+++ 09 - Lab - PlanetWars/sample/PlanetWars/bots/OneMove.py
@@ -2,9 +2,9 @@ class OneMove(object):
 
 	def update(self, gameinfo):
 
-		if gameinfo.my_planets and gameinfo.not_my_planets:
+		if gameinfo._my_planets and gameinfo._not_my_planets:
 			# print(gameinfo.my_planets)
-			src = list(gameinfo.my_planets.values())[0]
-			dest = list(gameinfo.not_my_planets.values())[0]
+			src = list(gameinfo._my_planets.values())[0]
+			dest = list(gameinfo._not_my_planets.values())[0]
 			gameinfo.planet_order(src, dest, src.num_ships)
 			gameinfo.log("I'll send %d ships from planet %s to planet %s" % (src.num_ships, src, dest))
