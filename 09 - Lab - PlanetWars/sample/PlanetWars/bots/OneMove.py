845880b855e8df12ae9a3e3e522d99087ab80af2
Finn Coffey <finn.coffey@gmail.com>
2024-05-14 16:18:48 +1000

    Fix errors when running OneMove bot

--- 09 - Lab - PlanetWars/sample/PlanetWars/bots/OneMove.py
+++ 09 - Lab - PlanetWars/sample/PlanetWars/bots/OneMove.py
@@ -2,9 +2,8 @@ class OneMove(object):
 
 	def update(self, gameinfo):
 
-		if gameinfo._my_planets and gameinfo._not_my_planets:
-			# print(gameinfo.my_planets)
-			src = list(gameinfo._my_planets.values())[0]
-			dest = list(gameinfo._not_my_planets.values())[0]
-			gameinfo.planet_order(src, dest, src.num_ships)
-			gameinfo.log("I'll send %d ships from planet %s to planet %s" % (src.num_ships, src, dest))
+		if gameinfo._my_planets() and gameinfo._not_my_planets():
+			src = list(gameinfo._my_planets().values())[0]
+			dest = list(gameinfo._not_my_planets().values())[0]
+			gameinfo.planet_order(src, dest, src.ships)
+			#gameinfo.log("I'll send %d ships from planet %s to planet %s" % (src.ships, src, dest))
